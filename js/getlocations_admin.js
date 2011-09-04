b3811c4204b224389ca5987ecfed94a5ca220f76
hutch <hutch@midwales.net>
2011-09-04 23:07:48 +0100

    improved return link, added user map, renamed some functions to avoid hook clashes

---
+++ js/getlocations_admin.js
@@ -0,0 +1,45 @@
+
+/**
+ * @file
+ * Javascript functions for getlocations module admin
+ *
+ * @author Bob Hutchinson http://drupal.org/user/52366
+ * jquery stuff
+*/
+(function ($) {
+
+  Drupal.behaviors.getlocations_admin = function() {
+
+    if ($("#edit-getlocations-default-returnlink-page-enable").attr('checked')) {
+      $("#wrap-page-link").show();
+    }
+    else {
+      $("#wrap-page-link").hide();
+    }
+    $("#edit-getlocations-default-returnlink-page-enable").change(function() {
+      if ($("#edit-getlocations-default-returnlink-page-enable").attr('checked')) {
+        $("#wrap-page-link").show();
+      }
+      else {
+        $("#wrap-page-link").hide();
+      }
+    });
+
+    if ($("#edit-getlocations-default-returnlink-user-enable").attr('checked')) {
+      $("#wrap-user-link").show();
+    }
+    else {
+      $("#wrap-user-link").hide();
+    }
+    $("#edit-getlocations-default-returnlink-user-enable").change(function() {
+      if ($("#edit-getlocations-default-returnlink-user-enable").attr('checked')) {
+        $("#wrap-user-link").show();
+      }
+      else {
+        $("#wrap-user-link").hide();
+      }
+    });
+
+  }
+
+})(jQuery);

4938fa8b62208abc962b0e20d29a94156e60b609
hutch <hutch@midwales.net>
2011-09-18 11:34:52 +0100

    fixed latlon check bug

--- getlocations-view-map.tpl.php
+++ getlocations-view-map.tpl.php
@@ -3,6 +3,7 @@
  * @file getlocations-view-map.tpl.php
  * Default simple view template to display a list of rows as icons on a map.
  * Derived from views-view-unformatted.tpl.php
+ * fed from function template_preprocess_getlocations_view_map
  *
  * @ingroup views_templates
  */

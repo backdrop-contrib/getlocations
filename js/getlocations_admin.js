4dce24901f198a3aafa346361dbaadd2b11eb6e4
hutch <hutch@midwales.net>
2011-12-22 14:17:04 +0000

    added term and comment maps, added default input marker

--- js/getlocations_admin.js
+++ js/getlocations_admin.js
@@ -41,6 +41,36 @@
         }
       });
 
+      if ($("#edit-getlocations-default-returnlink-term-enable").attr('checked')) {
+        $("#wrap-term-link").show();
+      }
+      else {
+        $("#wrap-term-link").hide();
+      }
+      $("#edit-getlocations-default-returnlink-term-enable").change(function() {
+        if ($(this).attr('checked')) {
+          $("#wrap-term-link").show();
+        }
+        else {
+          $("#wrap-term-link").hide();
+        }
+      });
+
+      if ($("#edit-getlocations-default-returnlink-comment-enable").attr('checked')) {
+        $("#wrap-comment-link").show();
+      }
+      else {
+        $("#wrap-comment-link").hide();
+      }
+      $("#edit-getlocations-default-returnlink-comment-enable").change(function() {
+        if ($(this).attr('checked')) {
+          $("#wrap-comment-link").show();
+        }
+        else {
+          $("#wrap-comment-link").hide();
+        }
+      });
+
       if ($("#edit-getlocations-colorbox-enable").attr('checked')) {
         $("#wrap-getlocations-colorbox").show();
       }

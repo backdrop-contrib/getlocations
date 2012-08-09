543aa21e575ec82f5588bf94737169f9fb521cbc
hutch <hutch@midwales.net>
2012-08-09 21:04:16 +0100

    better buttons

--- js/getlocations_views.js
+++ js/getlocations_views.js
@@ -55,6 +55,106 @@
         }
       });
 
+      if ($("#edit-style-options-trafficinfo").is('input')) {
+        if ($("#edit-style-options-trafficinfo").attr('checked')) {
+          $("#wrap-getlocations-trafficinfo").show();
+        }
+        else {
+          $("#wrap-getlocations-trafficinfo").hide();
+        }
+        $("#edit-style-options-trafficinfo").change(function() {
+          if ($(this).attr('checked')) {
+            $("#wrap-getlocations-trafficinfo").show();
+          }
+          else {
+            $("#wrap-getlocations-trafficinfo").hide();
+          }
+        });
+      }
+
+      if ($("#edit-style-options-bicycleinfo").is('input')) {
+        if ($("#edit-style-options-bicycleinfo").attr('checked')) {
+          $("#wrap-getlocations-bicycleinfo").show();
+        }
+        else {
+          $("#wrap-getlocations-bicycleinfo").hide();
+        }
+        $("#edit-style-options-bicycleinfo").change(function() {
+          if ($(this).attr('checked')) {
+            $("#wrap-getlocations-bicycleinfo").show();
+          }
+          else {
+            $("#wrap-getlocations-bicycleinfo").hide();
+          }
+        });
+      }
+
+      if ($("#edit-style-options-transitinfo").is('input')) {
+        if ($("#edit-style-options-transitinfo").attr('checked')) {
+          $("#wrap-getlocations-transitinfo").show();
+        }
+        else {
+          $("#wrap-getlocations-transitinfo").hide();
+        }
+        $("#edit-style-options-transitinfo").change(function() {
+          if ($(this).attr('checked')) {
+            $("#wrap-getlocations-transitinfo").show();
+          }
+          else {
+            $("#wrap-getlocations-transitinfo").hide();
+          }
+        });
+      }
+
+      if ($("#edit-style-options-panoramio-show").is('input')) {
+        if ($("#edit-style-options-panoramio-show").attr('checked')) {
+          $("#wrap-getlocations-panoramio").show();
+        }
+        else {
+          $("#wrap-getlocations-panoramio").hide();
+        }
+        $("#edit-style-options-panoramio-show").change(function() {
+          if ($(this).attr('checked')) {
+            $("#wrap-getlocations-panoramio").show();
+          }
+          else {
+            $("#wrap-getlocations-panoramio").hide();
+          }
+        });
+      }
+
+      if ($("#edit-style-options-weather-show").is('input')) {
+        if ($("#edit-style-options-weather-show").attr('checked')) {
+          $("#wrap-getlocations-weather").show();
+        }
+        else {
+          $("#wrap-getlocations-weather").hide();
+        }
+        $("#edit-style-options-weather-show").change(function() {
+          if ($(this).attr('checked')) {
+            $("#wrap-getlocations-weather").show();
+          }
+          else {
+            $("#wrap-getlocations-weather").hide();
+          }
+        });
+
+        if ($("#edit-style-options-weather-cloud").attr('checked')) {
+          $("#wrap-getlocations-weather-cloud").show();
+        }
+        else {
+          $("#wrap-getlocations-weather-cloud").hide();
+        }
+        $("#edit-style-options-weather-cloud").change(function() {
+          if ($(this).attr('checked')) {
+            $("#wrap-getlocations-weather-cloud").show();
+          }
+          else {
+            $("#wrap-getlocations-weather-cloud").hide();
+          }
+        });
+      }
+
   }
 
 })(jQuery);

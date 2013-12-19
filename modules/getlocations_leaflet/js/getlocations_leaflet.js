bbcf8d7f531d17cd7718bedc1f5666649a15c570
hutch <hutch@52366.no-reply.drupal.org>
2013-12-19 23:43:12 +0000

    added minimap plugin

--- modules/getlocations_leaflet/js/getlocations_leaflet.js
+++ modules/getlocations_leaflet/js/getlocations_leaflet.js
@@ -368,11 +368,15 @@ var getlocations_leaflet_layerscontrol = [];
         // Layer control
         if (map_settings.layerControl) {
           var layeropts = {};
-          if (map_settings.layercontrolposition) {
-            layeropts.position = map_settings.layercontrolposition;
+          if (map_settings.minimap) {
+            getlocations_leaflet_layerscontrol[key] = L.control.layers.minimap(layers, getlocations_leaflet_overlays[key], layeropts).addTo(getlocations_leaflet_map[key]);
           }
-          getlocations_leaflet_layerscontrol[key] = L.control.layers(layers, getlocations_leaflet_overlays[key], layeropts);
-          getlocations_leaflet_map[key].addControl(getlocations_leaflet_layerscontrol[key]);
+          else {
+            if (map_settings.layercontrolposition) {
+              layeropts.position = map_settings.layercontrolposition;
+            }
+            getlocations_leaflet_layerscontrol[key] = L.control.layers(layers, getlocations_leaflet_overlays[key], layeropts).addTo(getlocations_leaflet_map[key]);
+          }
         }
 
       } // end is there really a map?

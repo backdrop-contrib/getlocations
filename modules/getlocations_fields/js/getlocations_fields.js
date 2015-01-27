f461e6c3dfeaa846cc7e14f05f9ef0f773fb48ab
hutch <hutch@52366.no-reply.drupal.org>
2015-01-27 15:45:46 +0000

    more field_group support

--- modules/getlocations_fields/js/getlocations_fields.js
+++ modules/getlocations_fields/js/getlocations_fields.js
@@ -218,12 +218,29 @@
             }
           }
 
-          // field group multipage support
-          if ($(".multipage-link-next,.multipage-link-previous").is('input')) {
-            $(".multipage-link-next,.multipage-link-previous").one('click', function(event) {
-              Drupal.getlocations.redoMap(key);
-              Drupal.getlocations_inputmap[key].setCenter(point[key]);
-           });
+          // field_group support
+          if (gset.field_group_enable) {
+            // field group multipage support
+            if ($(".multipage-link-next,.multipage-link-previous").is('input')) {
+              $(".multipage-link-next,.multipage-link-previous").one('click', function(event) {
+                Drupal.getlocations.redoMap(key);
+                Drupal.getlocations_map[key].setCenter(point[key]);
+              });
+            }
+            // field group vert and horiz tabs
+            if ($(".vertical-tabs-list,.horizontal-tabs-list").is('ul')) {
+              $("li.vertical-tab-button a, li.horizontal-tab-button a").bind('click', function(event) {
+                Drupal.getlocations.redoMap(key);
+                Drupal.getlocations_map[key].setCenter(point[key]);
+              });
+            }
+            // field group accordion
+            if ($(".field-group-accordion").is('div')) {
+              $(".accordion-item").bind('click', function(event) {
+                Drupal.getlocations.redoMap(key);
+                Drupal.getlocations_map[key].setCenter(point[key]);
+              });
+            }
           }
 
         } // end is there really a map?
@@ -711,7 +728,7 @@
           else { m = maptype; }
           return m;
         }
-        
+
         // Clear form
         function clearForm(k) {
           $("#" + adrsfield + k).val("");
